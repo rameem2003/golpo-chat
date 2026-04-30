90eada5cf03b7305ac4d11af49a3eafffb803f90
Mahmood Hassan Rameem <rameem2019@gmail.com>
2026-04-30 09:58:02 +0600

    Initial commit with project setup and basic structure established.

---
+++ mobile/constants/Colors.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  light: {
+    primary: "#5A0FC8",
+    primaryLight: "#7B3FD3",
+    chatSender: "#AD87E4",
+    chatReceiver: "#DECFF4",
+    overlay: "#EFE7FA",
+  },
+  dark: {
+    primary: "#B694F0",
+    primaryLight: "#8D63D9",
+    chatSender: "#3A2A5F",
+    chatReceiver: "#241B38",
+    overlay: "#161022",
+  },
+};
